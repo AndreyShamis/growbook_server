398c1309d90f3df4f53c7489ea55131b1ff7270f
Andrey Shamis <lolnik@gmail.com>
2019-05-08 13:28:08 +0300

    Add user file, molnolog, swiftmailer
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I1b0d490cdd0b0de67f3f71a054837082afd35b7e

---
+++ src/Controller/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="user_index")
+     * @param UserRepository $userRepo
+     * @return Response
+     */
+    public function index(UserRepository $userRepo): Response
+    {
+        return $this->render('/user/index.html.twig', ['users' => $userRepo->findAll()]);
+    }
+
+    /**
+     * Finds and displays a Users  entity.
+     *
+     * @Route("/{id}", name="user_show", methods={"GET"})
+     * @param User $obj
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function showAction(User $obj): Response
+    {
+        // check for "edit" access: calls all voters
+        $this->denyAccessUnlessGranted('view', $obj);
+        return $this->render('/user/show.html.twig', array(
+            'user' => $obj,
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing User entity.
+     *
+     * @Route("/{id}/edit", name="user_edit", methods={"GET|POST"})
+     * @param Request $request
+     * @param User $obj
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @throws \LogicException
+     */
+    public function editAction(Request $request, User $obj, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // check for "edit" access: calls all voters
+        $this->denyAccessUnlessGranted('edit', $obj);
+        $current_user= $this->get('security.token_storage')->getToken()->getUser();
+        $can_change_permissions = $this->isGranted('ROLE_ADMIN');
+        $editForm = $this->get('form.factory')->create(UserType::class, $obj, array(
+            'edit_enabled' => true,
+            'current_user' => $current_user,
+            'can_change_permissions' => $can_change_permissions,
+        ));
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            /** @var User $edited_user */
+            $edited_user = $editForm->getData();
+            if ($edited_user->isLdapUser() === false && $edited_user->getPlainPassword() !== null && \strlen($edited_user->getPlainPassword()) > 2) {
+                $password = $passwordEncoder->encodePassword($edited_user, $edited_user->getPlainPassword());
+                $edited_user->setPassword($password);
+            }
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_edit', array('id' => $obj->getId()));
+        }
+
+        return $this->render('/user/edit.html.twig', array(
+            'user' => $obj,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Utils\RandomString;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must return null if the user is not found.
+     *
+     * @param string $username The username
+     *
+     * @return UserInterface|null
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function loadUserByUsername($username): ?UserInterface
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @param array $criteria
+     * @return User
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function create(array $criteria): User
+    {
+        $criteria['username'] = strtolower($criteria['username']);
+        $criteria['email'] = strtolower($criteria['email']);
+        $entity = $this->findOneBy(array('username' => $criteria['username']));
+        $entity_email = null;
+        if ($entity === null) {
+            $entity_email = $this->findOneBy(array('email' => $criteria['email']));
+        }
+
+        if (null === $entity) {
+            if ($entity_email !== null) {
+                // User added but not contains all fields - need to Update
+                $entity = $entity_email;
+            } else {
+                $entity = new User();
+            }
+            $entity->setUsername($criteria['username']);
+            $entity->setEmail($criteria['email']);
+            $entity->setFullName($criteria['fullName']);
+            $entity->setLastName($criteria['lastName']);
+            $entity->setFirstName($criteria['firstName']);
+            $entity->setAnotherId($criteria['anotherId']);
+            $entity->setMobile($criteria['mobile']);
+            $entity->setIsLdapUser($criteria['ldapUser']);
+            $entity->setPassword($criteria['dummyPassword']);
+            $this->_em->persist($entity);
+            $this->_em->flush($entity);
+        }
+
+        return $entity;
+    }
+
+    /**
+     * @param $email
+     * @param $fullName
+     * @return User
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function createByEmail($email, $fullName): User
+    {
+        $criteria = array();
+
+        $nameArray = explode(' ', $fullName);
+        $firstName = array_shift($nameArray);
+        $lastName = implode(' ', $nameArray);
+        $criteria['username'] = trim($fullName);
+        $criteria['email'] = strtolower(trim($email));
+        $criteria['fullName'] = trim($fullName);
+        $criteria['lastName'] = trim($lastName);
+        $criteria['firstName'] = trim($firstName);
+        $criteria['dummyPassword'] = RandomString::generateRandomString(20);
+
+        $entity = $this->findOneBy(array('email' => $criteria['email']));
+
+        if (null === $entity) {
+            $entity = new User();
+            $entity->setUsername($criteria['username']);
+            $entity->setEmail($criteria['email']);
+            $entity->setFullName($criteria['fullName']);
+            $entity->setLastName($criteria['lastName']);
+            $entity->setFirstName($criteria['firstName']);
+            $entity->setPassword($criteria['dummyPassword']);
+            $this->_em->persist($entity);
+            $this->_em->flush($entity);
+        }
+
+        return $entity;
+    }
+    // /**
+    //  * @return User[] Returns an array of User objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
