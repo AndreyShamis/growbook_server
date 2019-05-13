6dc281bf58610c7e2c28531056af803917456fe7
Andrey Shamis <lolnik@gmail.com>
2019-05-13 10:40:28 +0300

    Add relation
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: Iafa4a82295c569654658edf75c37b7d463138f45

---
+++ src/Entity/Sensor.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\SensorRepository")
+ */
+class Sensor
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uniqId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Plant", inversedBy="sensors")
+     */
+    private $Plant;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Event", mappedBy="sensor")
+     */
+    private $events;
+
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUniqId(): ?string
+    {
+        return $this->uniqId;
+    }
+
+    public function setUniqId(string $uniqId): self
+    {
+        $this->uniqId = $uniqId;
+
+        return $this;
+    }
+
+    public function getPlant(): ?Plant
+    {
+        return $this->Plant;
+    }
+
+    public function setPlant(?Plant $Plant): self
+    {
+        $this->Plant = $Plant;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Event[]
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(Event $event): self
+    {
+        if (!$this->events->contains($event)) {
+            $this->events[] = $event;
+            $event->setSensor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(Event $event): self
+    {
+        if ($this->events->contains($event)) {
+            $this->events->removeElement($event);
+            // set the owning side to null (unless already changed)
+            if ($event->getSensor() === $this) {
+                $event->setSensor(null);
+            }
+        }
+
+        return $this;
+    }
+}
