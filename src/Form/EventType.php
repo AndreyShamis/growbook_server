de3612ecacb15442417da86b3c3658a19a6f6985
Andrey Shamis <lolnik@gmail.com>
2019-05-13 15:07:50 +0300

    Change GUI, add links
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: Ica9c6643d6917bb7a9bc520447ef1badf7646ffb

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,12 +3,18 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\Events\EventHumidity;
+use App\Entity\Events\EventTemperature;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use App\Twig\AppExtension;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EventType extends AbstractType
@@ -24,13 +30,39 @@ class EventType extends AbstractType
             ])
             //->add('createdAt')
             //->add('updatedAt')
-            ->add('type', HiddenType::class, [
+                ;
+        $nulTransformer = new CallbackTransformer(
+            function ($input)
+            {
+                return $input;
+            },
+            function ($input)
+            {
+                return null;
+            }
+        );
+        if (get_class($builder->getData()) === Event::class) {
+            $builder
+                ->add('type', ChoiceType::class, [
 //                'widget' => 'single_text',
-                'data' => get_class($builder->getData()),
-                'empty_data' => get_class($builder->getData()),
-                'required' => false,
-                'attr'=>array('style'=>'display:none;')
-            ])
+                    'required' => true,
+                    'attr'=>array(
+                        'style'=>'font: 10px;'
+                    ),
+                    'choices' => $this->buildTypeChoices(),
+                ]);
+            $builder->get('type')->addModelTransformer($nulTransformer);
+        } else {
+            $builder
+                ->add('type', HiddenType::class, [
+//                'widget' => 'single_text',
+                    'data' => get_class($builder->getData()),
+                    'empty_data' => get_class($builder->getData()),
+                    'required' => false,
+                    'attr'=>array('style'=>'display:none;')
+                ]);
+        }
+        $builder
             ->add('value')
             ->add('note')
             ->add('plant')
@@ -46,5 +78,16 @@ class EventType extends AbstractType
         $resolver->setDefaults(array(
             'name' => ''
         ));
+    }
+
+    protected function buildTypeChoices(): array
+    {
+        $a = new AppExtension();
+        $classes = array(EventHumidity::class, EventTemperature::class);
+        $ret = array();
+        foreach ($classes as $class) {
+            $ret[$a->parseType($class)] = $class;
+        }
+        return $ret;
     }
 }
