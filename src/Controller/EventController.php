02e3a7b4201b74351040e07d0395403341f49fde
Andrey Shamis <lolnik@gmail.com>
2019-06-19 10:12:44 +0300

    temporary disable alert
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I9416d0058f2361a58b46fa3699c53ce9ba52f43b

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -238,7 +238,7 @@ class EventController extends AbstractController
                 }
             }
             if ($automatic) {
-                $logger->alert($message);
+                //$logger->alert($message);
 
                 return new Response($message, $status);
             }
