5e5e968a62c683ca90dfa8a1c0f43832b8b141b6
Andrey Shamis <lolnik@gmail.com>
2019-07-10 13:14:29 +0300

    add event feed
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I44cc823201a58f825a55f482c7d68006386924ff

---
+++ src/Controller/Events/EventFeedController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller\Events;
+
+use App\Entity\Events\EventFeed;
+use App\Form\Events\EventFeedType;
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/events/event/feed")
+ */
+class EventFeedController extends AbstractController
+{
+    /**
+     * @Route("/", name="events_event_feed_index", methods={"GET"})
+     */
+    public function index(EventRepository $eventRepository): Response
+    {
+        return $this->render('events/event_feed/index.html.twig', [
+            'event_feeds' => $eventRepository->findOneBy(['type' => EventFeed::class]),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="events_event_feed_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $eventFeed = new EventFeed();
+        $form = $this->createForm(EventFeedType::class, $eventFeed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($eventFeed);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('events_event_feed_index');
+        }
+
+        return $this->render('events/event_feed/new.html.twig', [
+            'event_feed' => $eventFeed,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="events_event_feed_show", methods={"GET"})
+     */
+    public function show(EventFeed $eventFeed): Response
+    {
+        return $this->render('events/event_feed/show.html.twig', [
+            'event_feed' => $eventFeed,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="events_event_feed_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, EventFeed $eventFeed): Response
+    {
+        $form = $this->createForm(EventFeedType::class, $eventFeed);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('events_event_feed_index', [
+                'id' => $eventFeed->getId(),
+            ]);
+        }
+
+        return $this->render('events/event_feed/edit.html.twig', [
+            'event_feed' => $eventFeed,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="events_event_feed_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, EventFeed $eventFeed): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$eventFeed->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($eventFeed);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('events_event_feed_index');
+    }
+}
---
+++ src/Form/Events/EventFeedType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form\Events;
+
+use App\Entity\Events\EventFeed;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventFeedType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('plant')
+            ->add('water',null, ['required' => true, 'empty_data' => '1', 'help' => 'Liters, 100ml=0.1L'])
+            ->add('ph', null, ['required' => false, 'empty_data' => '6.0', 'help' => '[0.0-14.0]'])
+            ->add('tds')
+            ->add('ec')
+            ->add('temperature')
+            ->add('name')
+//            ->add('createdAt')
+//            ->add('updatedAt')
+            ->add('type')
+            ->add('value')
+            ->add('note')
+            ->add('value1')
+            ->add('value2')
+            ->add('value3')
+//            ->add('ip')
+
+//            ->add('sensor')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => EventFeed::class,
+        ]);
+    }
+}
