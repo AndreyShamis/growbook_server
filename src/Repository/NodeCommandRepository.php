180e8fd1c7964bc51d209d7308ef200168f5f002
Andrey Shamis <lolnik@gmail.com>
2019-12-04 14:41:43 +0200

    Move from 4.3 to 4.4
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I820e18029649a10659347251156def3b95d84113

--- src/Repository/NodeCommandRepository.php
+++ src/Repository/NodeCommandRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\NodeCommand;
 use App\Entity\Plant;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method NodeCommand|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class NodeCommandRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, NodeCommand::class);
     }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use App\Utils\RandomString;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class UserRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
