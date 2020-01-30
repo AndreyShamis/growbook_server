2d81d8dad65f0b4b6755c2169c680456d45bd368
Andrey Shamis <andrey.shamis@intel.com>
2020-01-30 14:23:17 +0200

    Posible fix for graph
    Change-Id: I2c547ab319bb2c9ce6e39ec586292eadcbcc1cdb
    Signed-off-by: Andrey Shamis <andrey.shamis@intel.com>

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,15 +1,9 @@
 function floorDate(datetime) {
-    var newDate = new Date(datetime);
+    var newDate = new Date(Date.UTC(datetime.getFullYear(), datetime.getMonth()-1, datetime.getDate(), datetime.getHours()));
     //newDate.setHours(0);
-    newDate.setMonth(newDate.getMonth() -1);    // Fix issue https://github.com/google/google-visualization-issues/issues/1058
-    // if (newDate.getMinutes() > 30 && newDate.getHours() < 23) {
-    //     console.log(newDate.getHours());
-    //     newDate.setHours(newDate.getHours() + 1);
-    //     console.log(newDate.getHours());
-    //     console.log('');
-    // }
-    newDate.setMinutes(0);
-    newDate.setSeconds(0);
+    //newDate.setMonth(newDate.getMonth() -1);    // Fix issue https://github.com/google/google-visualization-issues/issues/1058
+    // newDate.setMinutes(0);
+    // newDate.setSeconds(0);
     return newDate;
 }
 
